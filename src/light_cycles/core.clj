e32b2f922178e6c44ecf7924d37dbe572c4ab4d9
jarl-haggerty <jarl-haggerty@jarlhaggerty-home.(none)>
2011-07-06 01:59:02 -0700

    network communications seems complete

--- src/light_cycles/core.clj
+++ src/light_cycles/core.clj
@@ -1,42 +1,55 @@
 (ns light-cycles.server
   (:import java.io.DataInputStream
 	   java.io.DataOutputStream
-	   java.net.Socket)
+	   java.net.Socket
+	   javax.swing.Timer
+	   java.awt.event.ActionListener)
   (:use clojure.contrib.server-socket)
   (:require [light-cycles.network :as network]))
+
+(def info-header 0)
+(def connect-header 1)
+(def request-header 2)
+(def input-header 2)
+
+(def state (atom {}))
+
+(def running (atom true))
+
+(def timer (doto (Timer. 1000
+			 (proxy [ActionListener] []
+			   (actionPerformed [_]
+					    (swap! running not))))
+	     (.setRepeats false)
+	     (.start)))
+
+(defn game-loop []
+  (loop []
+    (condp = (.readByte network/input)
+	request-header ()
+	input-header ())
+    (if @running
+      (recur))))
+
+(defn send-info []
+  (network/send {:game "Light Cycles" :player 20})
+  (println 'server (persistent! network/codec)))
 
 (defn start [port]
   (let [connections (atom [])]
     (create-server port (fn [in out]
-			  (swap! connections #(conj % out))
-			  (let [in (DataInputStream. in)
-				out (DataOutputStream. out)]
-			    (loop []
-			      (dotimes [_ (.readByte in)]
-				(condp = (.readByte in)
-				    network/keyword-header (do (println "read keyword-header") (network/receive-keyword in out))
-				    network/string-header (let [data (byte-array (.readInt in))]
-							    (loop [accum 0]
-							      (when (< accum (alength data))
-								(recur (+ accum (doto (.read in data accum (- (alength data) accum))
-										  (println))))))
-							    (String. data))
-				    network/int-header (do (println "read int-header") (.readInt in))
-				    network/float-header (do (println "read float-header") (.readFloat in))))
-			      (recur)))))))
-(comment (binding [network/codec (atom {})]
-	   (start 4000)
+			  (binding [network/input (DataInputStream. in)
+				    network/output (DataOutputStream. out)
+				    network/codec (transient {})]
+			    (condp = (.readByte network/input) 
+				info-header (send-info)
+				connect-header (game-loop)))))))
 
-	   (def socket (Socket. "localhost" 4000))
-	   (def in (DataInputStream. (.getInputStream socket)))
-	   (def out (DataOutputStream. (.getOutputStream socket)))
-	   (.writeByte out 6)
-	   (.writeByte out network/int-header)
-	   (.writeInt out 45)
-	   (network/send-keyword :foo in out)
-	   (.writeByte out network/float-header)
-	   (.writeFloat out 34.875)
-	   (network/send-keyword :bar in out)
-	   (network/send-keyword :foo in out)
-	   (network/send-keyword :hello in out)
-	   (println @light-cycles.network/codec)))
+(start 4000)
+(def socket (Socket. "localhost" 4000))
+(binding [network/input (DataInputStream. (.getInputStream socket))
+	  network/output (DataOutputStream. (.getOutputStream socket))
+	  network/codec (transient {})]
+  (.writeByte network/output info-header)
+  (println 'hello (network/receive))
+  (println 'client (persistent! network/codec)))
--- src/light_cycles/network.clj
+++ src/light_cycles/network.clj
@@ -6,63 +6,97 @@
 (def string-header 3)
 (def int-header 4)
 (def float-header 5)
+(def map-header 6)
+(def more-header 7)
+(def done-header 8)
 
-(def codec (atom {}))
+(def codec)
+(def input)
+(def output)
 
-(defn send-keyword [keyword input output]
-  (.writeByte output keyword-header)
-  (comment (println "sent keyword-header"))
-  (if-let [code (@codec keyword)]
-    (do (.writeByte output encoded-header)
-	(.writeByte output code))
-    (let [name (name keyword)
-	  length (count name)]
-      (.writeByte output raw-header)
-      (comment (println "sent raw-header"))
-      (.writeByte output length)
-      (comment (println "sent length"))
-      (.write output (.getBytes name) 0 length)
-      (println "sent keyword name" (seq (.getBytes name)))
-      (let [code (.readByte input)]
-	(comment (println "read code"))
-	(swap! codec #(assoc % keyword code code keyword))))))
+					;---------------------------send-------------------------------------------
+(def send)
+(defn send-int [int]
+  (.writeByte output int-header)
+  (.writeInt output int))
 
-(defn receive-keyword [input output]
-  (if (= raw-header (.readByte input))
-    (do    (println "read raw-header")
-	   (let [data (byte-array (.readByte input))
-		 _ (println "read keyword length" (alength data))
-		 _ (loop [accum 0]
-		     (println "reading" (String. data) accum (alength data))
-		     (when (< accum (alength data))
-		       (recur (+ accum (doto (.read input data accum (- (alength data) accum))
-					 (println))))))
-		 _ (println "read keyword name" (String. data) (seq data))
-		 keyword (keyword (String. data))]
-	     (if-let [code (@codec keyword)]
-	       (.writeByte output code)
-	       (do (.writeByte output ((swap! codec #(assoc % keyword (/ (count %) 2) (/ (count %) 2) keyword)) keyword))
-		   (println "sent code")))))
-    (@codec (.readByte input))))
+(defn send-float [float]
+  (.writeByte output float-header)
+  (.writeFloat output float))
 
-(defn send-string [string output]
+(defn send-string [string]
   (.writeByte output string-header)
   (.writeInt output (count string))
   (.write output (.getBytes string)))
 
-(defn send-int [int output]
-  (.writeByte output int-header)
-  (.writeInt output int))
+(defn send-keyword [keyword]
+  (.writeByte output keyword-header)
+  (if-let [code (codec keyword)]
+    (do (.writeByte output encoded-header)
+	(.writeByte output code)
+	codec)
+    (let [name (name keyword)
+	  length (count name)]
+      (.writeByte output raw-header)
+      (.writeByte output length)
+      (.write output (.getBytes name) 0 length)
+      (assoc! codec keyword (/ (count codec) 2) (/ (count codec) 2) keyword))))
 
-(defn send-float [float output]
-  (.writeByte output float-header)
-  (.writeFloat output float))
+(defn send-map [map]
+  (.writeByte output map-header)
+  (doseq [[k v] map]
+    (.writeByte output more-header)
+    (send k)
+    (send v))
+  (.writeByte output done-header))
 
-(defn send [output input & transmission]
-  (.writeByte output input (count transmission))
-  (doseq [piece transmission]
-    (cond
-     (keyword? piece) (send-keyword piece input output)
-     (string? piece) (send-string piece output)
-     (integer? piece) (send-int piece output)
-     (float? piece) (send-float piece output))))
+(defn send [item]
+  (cond
+   (map? item) (send-map item)
+   (keyword? item) (send-keyword item)
+   (string? item) (send-string item)
+   (integer? item) (send-int item)
+   (float? item) (send-float item)))
+
+
+					;---------------------------receive-------------------------------------------
+(def receive)
+(defn receive-int []
+  (.readInt input))
+
+(defn receive-float []
+  (.readFloat input))
+
+(defn receive-string []
+  (let [data (byte-array (.readInt input))]
+    (loop [accum 0]
+      (when (< accum (alength data))
+	(recur (+ accum (.read input data accum (- (alength data) accum))))))
+    (String. data)))
+
+(defn receive-keyword []
+  (if (= encoded-header (.readByte input))
+    (codec (.readByte input))
+    (let [data (byte-array (.readByte input))
+	  _ (loop [accum 0]
+	      (when (< accum (alength data))
+		(recur (+ accum (.read input data accum (- (alength data) accum))))))
+	  keyword (keyword (String. data))]
+      (assoc! codec keyword (/ (count codec) 2) (/ (count codec) 2) keyword)
+      keyword)))
+
+(defn receive-map []
+  (loop [accum {}]
+    (if (= (.readByte input) more-header)
+      (let [key (receive)
+	    value (receive)]
+	(recur (assoc accum key value)))
+      accum)))
+
+(defn receive []
+  (condp = (.readByte input)
+      map-header (receive-map)
+      keyword-header (receive-keyword)
+      string-header (receive-string)
+      int-header (receive-int)
+      float-header (receive-float)))
